bcc8e6b629349a9bb130dbf7ce233194d9ac68a6
Alexander Colen <alexanderc@live.nl>
2021-12-09 13:52:55 -0500

    Day09 - Puzzle 1

---
+++ AdventOfCode2021/Day09/Puzzle1.cs
@@ -0,0 +1,81 @@
+﻿using AdventOfCode2021.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2021.Day09
+{
+    class Puzzle1 : IPuzzle
+    {
+        public Puzzle1()
+        {
+            Console.WriteLine("\nDay 09 - Puzzle 1");
+            RunPuzzle();
+        }
+
+        public void RunPuzzle()
+        {
+            using StreamReader sr = new StreamReader(@"Day09/input.txt");
+            var grid = new List<int[]>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var row = new int[line.Length];
+                for (var x = 0; x < line.Length; x++)
+                {
+                    row[x] = int.Parse(line[x].ToString());
+                }
+                grid.Add(row);
+            }
+
+            var risk = 0;
+            for (var y = 0; y < grid.Count; y++)
+            {
+                for (var x = 0; x < grid[y].Length; x++)
+                {
+                    // Left
+                    if (x > 0)
+                    {
+                        if (grid[y][x - 1] <= grid[y][x])
+                        {
+                            continue;
+                        }
+                    }
+
+                    // Right
+                    if (x < grid[y].Length - 1)
+                    {
+                        if (grid[y][x + 1] <= grid[y][x])
+                        {
+                            continue;
+                        }
+                    }
+
+                    // Above
+                    if (y > 0)
+                    {
+                        if (grid[y - 1][x] <= grid[y][x])
+                        {
+                            continue;
+                        }
+                    }
+
+                    // Below
+                    if (y < grid.Count - 1)
+                    {
+                        if (grid[y + 1][x] <= grid[y][x])
+                        {
+                            continue;
+                        }
+                    }
+
+                    // Reaching this means all neighbours are higher.
+                    risk += 1 + grid[y][x];
+                }
+            }
+
+            Console.WriteLine($"Outcome: {risk}");
+        }
+    }
+}
---
+++ AdventOfCode2021/Day09/Puzzle2.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode2021.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2021.Day09
+{
+    class Puzzle2 : IPuzzle
+    {
+        public Puzzle2()
+        {
+            Console.WriteLine("\nDay 09 - Puzzle 2");
+            RunPuzzle();
+        }
+
+        public void RunPuzzle()
+        {
+            using StreamReader sr = new StreamReader(@"Day09/input.txt");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                
+            }
+
+            Console.WriteLine($"Outcome: {"Not implemented"}");
+        }
+    }
+}
