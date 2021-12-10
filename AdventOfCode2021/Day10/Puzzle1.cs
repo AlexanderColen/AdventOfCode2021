e676a3515e181404ed1dbaf86d83a5167e2ae5fd
Alexander Colen <alexanderc@live.nl>
2021-12-10 13:09:34 -0500

    Day10 - Puzzles 1 + 2

---
+++ AdventOfCode2021/Day10/Puzzle1.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode2021.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2021.Day10
+{
+    class Puzzle1 : IPuzzle
+    {
+        public Puzzle1()
+        {
+            Console.WriteLine("\nDay 10 - Puzzle 1");
+            RunPuzzle();
+        }
+
+        public void RunPuzzle()
+        {
+            using StreamReader sr = new StreamReader(@"Day10/input.txt");
+            var openingChars = new char[4] { '(', '[', '{', '<' };
+            var closingChars = new char[4] { ')', ']', '}', '>' };
+            var illegalChars = new List<char>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var openChunks = new List<char>();
+                foreach (var c in line)
+                {
+                    if (openingChars.Contains(c))
+                    {
+                        openChunks.Add(c);
+                    } else if (closingChars.Contains(c))
+                    {
+                        if (Array.FindIndex(closingChars, x => x == c) == Array.FindIndex(openingChars, x => x == openChunks[^1]))
+                        {
+                            openChunks = openChunks.SkipLast(1).ToList();
+                        } else
+                        {
+                            illegalChars.Add(c);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Outcome: {illegalChars.Count(x => x == ')') * 3 + illegalChars.Count(x => x == ']') * 57 + illegalChars.Count(x => x == '}') * 1197 + illegalChars.Count(x => x == '>') * 25137}");
+        }
+    }
+}
---
+++ AdventOfCode2021/Day10/Puzzle2.cs
@@ -0,0 +1,89 @@
+﻿using AdventOfCode2021.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2021.Day10
+{
+    class Puzzle2 : IPuzzle
+    {
+        public Puzzle2()
+        {
+            Console.WriteLine("\nDay 10 - Puzzle 2");
+            RunPuzzle();
+        }
+
+        public void RunPuzzle()
+        {
+            using StreamReader sr = new StreamReader(@"Day10/input.txt");
+            var openingChars = new char[4] { '(', '[', '{', '<' };
+            var closingChars = new char[4] { ')', ']', '}', '>' };
+            var incompleteChunkChars = new List<List<char>>();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                var openChunks = new List<char>();
+                var corrupted = false;
+                foreach (var c in line)
+                {
+                    if (openingChars.Contains(c))
+                    {
+                        openChunks.Add(c);
+                    } else if (closingChars.Contains(c))
+                    {
+                        if (Array.FindIndex(closingChars, x => x == c) == Array.FindIndex(openingChars, x => x == openChunks[^1]))
+                        {
+                            openChunks = openChunks.SkipLast(1).ToList();
+                        } else
+                        {
+                            corrupted = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!corrupted)
+                {
+                    incompleteChunkChars.Add(openChunks);
+                }
+            }
+
+            var scores = new List<long>();
+            
+            foreach (var chunk in incompleteChunkChars)
+            {
+                long score = 0;
+
+                chunk.Reverse();
+
+                foreach (var c in chunk)
+                {
+                    var closingChar = closingChars[Array.FindIndex(openingChars, x => x == c)];
+                    score *= 5;
+
+                    switch (closingChar)
+                    {
+                        case ')':
+                            score += 1;
+                            break;
+                        case ']':
+                            score += 2;
+                            break;
+                        case '}':
+                            score += 3;
+                            break;
+                        case '>':
+                            score += 4;
+                            break;
+                    }
+                }
+
+                scores.Add(score);
+            }
+            scores.Sort();
+
+            Console.WriteLine($"Outcome: {scores[scores.Count / 2]}");
+        }
+    }
+}
